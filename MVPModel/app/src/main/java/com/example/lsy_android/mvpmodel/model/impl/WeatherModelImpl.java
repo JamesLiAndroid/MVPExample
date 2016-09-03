7000a7fd873ae34b143e2e0f8c156497de111c8e
lsy-android <lsy1234567@163.com>
2016-09-03 15:22:27 +0800

    删除未使用的BodyString类

--- MVPModel/app/src/main/java/com/example/lsy_android/mvpmodel/model/impl/WeatherModelImpl.java
+++ MVPModel/app/src/main/java/com/example/lsy_android/mvpmodel/model/impl/WeatherModelImpl.java
@@ -6,7 +6,6 @@ import com.example.lsy_android.mvpmodel.model.OnWeatherListener;
 import com.example.lsy_android.mvpmodel.model.WeatherModel;
 import com.example.lsy_android.mvpmodel.model.entity.Weather;
 import com.example.lsy_android.mvpmodel.network.NetworkService;
-import com.google.gson.Gson;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -19,23 +18,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
  */
 public class WeatherModelImpl implements WeatherModel {
 
-    private Gson gson = new Gson();
-
     @Override
     public void loadWeather(String cityName, final OnWeatherListener litener) {
-
-        /*List<String> cityNames = new ArrayList<String>();
-        cityNames.add(cityName);
-
-        Map<String, Object> map = new HashMap<>();
-        map.put("accessId","BHN5QW5KCM9PZDE0NZIYODM5MDG0MDE=");
-        map.put("citynames", cityNames);*/
-
-       /*BodyString bodyString = new BodyString();
-        bodyString.setAccessId("BHN5QW5KCM9PZDE0NZIYODM5MDG0MDE=");
-        bodyString.setCitynames(cityNames);*/
-       /* String entity = gson.toJson(map);
-        Log.d("TAG", "拼接的数据内容为："+entity.toString());*/
 
         // 进行网络请求，请求数据
         Retrofit retrofit = new Retrofit.Builder()
@@ -68,51 +52,5 @@ public class WeatherModelImpl implements WeatherModel {
                 Log.e("TAG", "onFailure=" + t.getMessage());
             }
         });
-        /**
-         * 返回虚拟数据
-         * {
-         "resultCode": "0",
-         "resultDesc": "Success",
-         "info": {
-         "citycode": "101120201",
-         "publishTime": "2016-08-17",
-         "forecast": [
-         {
-         "hightemp": "30℃",
-         "fengxiang": "北风",
-         "lowtemp": "26℃",
-         "fengli": "3-4级",
-         "date": "2016-08-17",
-         "type": "多云",
-         "week": "星期三"
-         }
-         ],
-         "currentTemp": "29.3",
-         "cityname": "青岛"
-         }
-         }
-         */
-        String result = "{\n" +
-                "    \"resultCode\": \"0\",\n" +
-                "    \"resultDesc\": \"Success\",\n" +
-                "    \"info\": {\n" +
-                "        \"citycode\": \"101120201\",\n" +
-                "        \"publishTime\": \"2016-08-17\",\n" +
-                "        \"forecast\": [\n" +
-                "            {\n" +
-                "                \"hightemp\": \"30℃\",\n" +
-                "                \"fengxiang\": \"北风\",\n" +
-                "                \"lowtemp\": \"26℃\",\n" +
-                "                \"fengli\": \"3-4级\",\n" +
-                "                \"date\": \"2016-08-17\",\n" +
-                "                \"type\": \"多云\",\n" +
-                "                \"week\": \"星期三\"\n" +
-                "            }  \n" +
-                "      ],\n" +
-                "        \"currentTemp\": \"29.3\",\n" +
-                "        \"cityname\": \"青岛\"\n" +
-                "    }\n" +
-                "}";
-
     }
 }
